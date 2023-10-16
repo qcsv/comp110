845bf63d0da63782b9ddf87145d6ff95ff9c1690
crqst <145231886+crqst@users.noreply.github.com>
2023-10-16 12:53:03 -0700

    working double triangle

--- projects/initalDiamond/src/initalDiamond.java
+++ projects/initalDiamond/src/initalDiamond.java
@@ -7,20 +7,52 @@ public class initalDiamond
 {
 	public static void main(String[] args)
 	{
-		Random rand = new Random();
-		int n = rand.nextInt(31 -11) + 11;
-		int row;
 		System.out.println("RISHI CUTCHIN");
+		//random number generation
+		Random rand = new Random();
+		int n = rand.nextInt(31 - 11) + 11;
+		if (n % 2 == 0)
+		{
+		      	n++; //if n is even, icrement by one
+		}
+		System.out.printf("Number of lines: %d\n", n);
+		
+
+
+		//first triangle
 		for (int x = 1; x < n; x++)
 		{ 
-			for (int y = 0; y < x; y++)
+			for (int bl = 0; bl < n - x; bl++)
+			{
+				System.out.print(" ");
+			}
+			for (int y = 0; y < (x * 2) - 1; y++)
 			{
 				System.out.print("*");
 			}
 			System.out.print("\n");
 		}
-		//end square loop
-		System.out.println("RISHI CUTCHIN");
-		
+
+		//barrier line
+		for (int l = 0; l < n * 2; l++)
+		{
+			System.out.print("=");
+		}
+		System.out.print("\n");
+
+
+		for (int x = n; x >= 1; x--)
+		{ 
+			for (int bl = 0; bl < n - x; bl++)
+			{
+				System.out.print(" ");
+			}
+			for (int y = 0; y < (x * 2) - 1; y++)
+			{
+				System.out.print("*");
+			}
+			System.out.print("\n");              		
+		}
+
 	}
 }
