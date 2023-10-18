eb093a5be36f95c027ae7dde68480fdad1c51743
crqst <145231886+crqst@users.noreply.github.com>
2023-10-18 12:27:42 -0700

    license

--- exercises/ch7/grades/src/gradeCheck.java
+++ exercises/ch7/grades/src/gradeCheck.java
@@ -1,3 +1,12 @@
+/*
+    gradeCheck.java - Accept Input and Check Grades
+
+    Written in 2023 by Rishi Cutchin rishincutchin@gmail.com
+
+    To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+
+    You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>. 
+*/
 import java.util.Scanner;
 import java.util.Arrays; 
 public class gradeCheck
--- projects/nameSwitcher/src/nameSwitcher.java
+++ projects/nameSwitcher/src/nameSwitcher.java
@@ -1,6 +1,12 @@
-// nameSwitcher.java
-// Some strange switching of first last and middle name
-// Author: RC
+/*
+    nameSwitcher.java - some strange switching of first last and middle name
+
+    Written in 2023 by Rishi Cutchin rishincutchin@gmail.com
+
+    To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+
+    You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>. 
+*/
 import java.util.Scanner;
 public class nameSwitcher
 {
